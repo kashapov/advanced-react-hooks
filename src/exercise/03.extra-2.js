7c43f5d8ca5431fc302ca38aadcf8b129aaa646d
Andrey Kashapov <kashapovandrey@gmail.com>
2021-01-28 22:13:06 +0200

    Complete exercise 3-2

--- src/exercise/03.extra-2.js
+++ src/exercise/03.extra-2.js
@@ -2,9 +2,6 @@
 // 💯 caching in a context provider (exercise)
 // http://localhost:3000/isolated/exercise/03.extra-2.js
 
-// you can edit this here and look at the isolated page or you can copy/paste
-// this in the regular exercise file.
-
 import * as React from 'react'
 import {
   fetchPokemon,
@@ -15,14 +12,7 @@ import {
 } from '../pokemon'
 import {useAsync} from '../utils'
 
-// 🐨 Create a PokemonCacheContext
-
-// 🐨 create a PokemonCacheProvider function
-// 🐨 useReducer with pokemonCacheReducer in your PokemonCacheProvider
-// 💰 you can grab the one that's in PokemonInfo
-// 🐨 return your context provider with the value assigned to what you get back from useReducer
-// 💰 value={[cache, dispatch]}
-// 💰 make sure you forward the props.children!
+const PokemonCacheContext = React.createContext();
 
 function pokemonCacheReducer(state, action) {
   switch (action.type) {
@@ -35,12 +25,21 @@ function pokemonCacheReducer(state, action) {
   }
 }
 
-function PokemonInfo({pokemonName}) {
-  // 💣 remove the useReducer here (or move it up to your PokemonCacheProvider)
-  const [cache, dispatch] = React.useReducer(pokemonCacheReducer, {})
-  // 🐨 get the cache and dispatch from useContext with PokemonCacheContext
+function PokemonCacheProvider(props) {
+  const [cache, dispatch] = React.useReducer(pokemonCacheReducer, {});
+  return <PokemonCacheContext.Provider value={[cache, dispatch]} {...props} />
+}
 
-  const {data: pokemon, status, error, run, setData} = useAsync()
+function usePokemonCache() {
+  const context = React.useContext(PokemonCacheContext);
+  if(!context) throw new Error('usePokemonCache must be used within PokemonCacheContext');
+  return context;
+}
+
+function PokemonInfo({pokemonName}) {
+  const [cache, dispatch] = usePokemonCache();
+
+  const {data: pokemon, status, error, run, setData} = useAsync();
 
   React.useEffect(() => {
     if (!pokemonName) {
@@ -50,12 +49,12 @@ function PokemonInfo({pokemonName}) {
     } else {
       run(
         fetchPokemon(pokemonName).then(pokemonData => {
-          dispatch({type: 'ADD_POKEMON', pokemonName, pokemonData})
+          dispatch({type: 'ADD_POKEMON', pokemonName, pokemonData});
           return pokemonData
         }),
       )
     }
-  }, [cache, pokemonName, run, setData])
+  }, [cache, dispatch, pokemonName, run, setData]);
 
   if (status === 'idle') {
     return 'Submit a pokemon'
@@ -69,8 +68,8 @@ function PokemonInfo({pokemonName}) {
 }
 
 function PreviousPokemon({onSelect}) {
-  // 🐨 get the cache from useContext with PokemonCacheContext
-  const cache = {}
+  const [cache] = usePokemonCache();
+
   return (
     <div>
       Previous Pokemon
@@ -91,25 +90,25 @@ function PreviousPokemon({onSelect}) {
 }
 
 function PokemonSection({onSelect, pokemonName}) {
-  // 🐨 wrap this in the PokemonCacheProvider so the PreviousPokemon
-  // and PokemonInfo components have access to that context.
   return (
     <div style={{display: 'flex'}}>
-      <PreviousPokemon onSelect={onSelect} />
-      <div className="pokemon-info" style={{marginLeft: 10}}>
-        <PokemonErrorBoundary
-          onReset={() => onSelect('')}
-          resetKeys={[pokemonName]}
-        >
-          <PokemonInfo pokemonName={pokemonName} />
-        </PokemonErrorBoundary>
-      </div>
+      <PokemonCacheProvider>
+        <PreviousPokemon onSelect={onSelect} />
+        <div className="pokemon-info" style={{marginLeft: 10}}>
+          <PokemonErrorBoundary
+            onReset={() => onSelect('')}
+            resetKeys={[pokemonName]}
+          >
+            <PokemonInfo pokemonName={pokemonName} />
+          </PokemonErrorBoundary>
+        </div>
+      </PokemonCacheProvider>
     </div>
   )
 }
 
 function App() {
-  const [pokemonName, setPokemonName] = React.useState(null)
+  const [pokemonName, setPokemonName] = React.useState(null);
 
   function handleSubmit(newPokemonName) {
     setPokemonName(newPokemonName)
